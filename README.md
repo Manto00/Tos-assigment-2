807fed7cc346e762796f1f26ddd048e24b01cea1
Manto00 <mantoangabriele00@gmail.com>
2022-05-20 15:06:01 +0200

    Initial commit
    Set up repository

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Tos-assigment-2
-Repo per il secondo assigment di metodi e tecnologie per lo sviluppo software
+![Coverage](.github/badges/jacoco.svg)
+![Branches](.github/badges/branches.svg)
+# Assignment-elettronica-2
+Questa repo è destinata al secondo assignment del corso di metodi e tecnologie per lo sviluppo software
---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [GABRIELE] [MANTOAN] [1216746]
+// [RAFFAELE] [OLIVIERO] [1216748]
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
